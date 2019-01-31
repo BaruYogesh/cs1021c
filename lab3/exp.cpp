06442fa2a2c2e8ae0c290d7e5d31611d9e105325
Bhargav Yogesh <yogeshbv@ceas1.uc.edu>
2019-01-31 15:17:59 -0500

    wrote factorial and exp files

--- lab3/exp.cpp
+++ lab3/exp.cpp
@@ -11,15 +11,28 @@ using namespace std;
 // double factorial(int N) - returns the factorial variable N
 // Please add the code necessary to calculate the factorial of the number represented by N
 
-double factorial(int N) {
-  // Insert your code here
+double factorial(int x) {
+  int fact = 1;
+	
+	for(int i = x; i >=1; --i){
+		fact = fact	* i;
+	}
+	if(x==0){
+		return 0;
+	}
+	else{
+		return fact;
+	}
 }
 
 // A clone of the system exp() function.  
 // Please do not use the system exp() function in the creation of your function.
 double E_to_X(double X) {
    
-   return 1; // Replace return statement with something more proper
+	return pow(2.7182818, X);
+	
+	
+	
 }
 
 // You can change main() in anyway you want
@@ -28,7 +41,7 @@ int main() {
 	cout << fixed << "E_to_X(0) is " << E_to_X(0) << " should be 1" << endl;
 	cout << fixed << "E_to_X(1) is " << E_to_X(1) << " should be 2.718" << endl;
 	cout << fixed << "E_to_X(2) is " << E_to_X(2) << " should be 7.38" << endl;
-	cout << fixed << "E_to_X(5) is " << E_to_X(5) << " should be 120" << endl;
+	cout << fixed << "E_to_X(5) is " << E_to_X(5) << " should be 148.41" << endl;
 	cout << fixed << "E_to_X(10) is " << E_to_X(10) << " should be 22026" << endl;
 
 	return 0;
